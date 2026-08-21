33979c31d5c79b6f05fdc07508765016e96aa598
RusProgger <ruslantimka92@gmail.com>
2026-08-21 22:14:32 +0300

    Add MethodsOutsideClass

--- 02_method/MethodsOutsideClass.cpp
+++ 02_method/MethodsOutsideClass.cpp
@@ -3,29 +3,9 @@
 #include <print>
 
 
-// Cоздайте класс Person, cоздайте обьект класса Alex и через метод выведите  "привет Alex";
-
-class Person {
-    public:
-        std::string name;
-
-    // Обьявление метода
-
-    void printMessage() {
-        std::print("Привет {}", name);
-    }
-};
 
 int main() {
 
-    // Обьявление обекта класса 
-
-    Person Alex;
-    Alex.name = "Alex";
-
-    Alex.printMessage();
-
-
-    std::print("C++ 2023!!"); 
+    
     return 0;
 }
