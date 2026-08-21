5de5821a64365cc4321104d21694ef61d771c329
RusProgger <ruslantimka92@gmail.com>
2026-08-21 22:56:08 +0300

    Add user input

--- 02_method/MethodsOutsideClass.cpp
+++ 02_method/MethodsOutsideClass.cpp
@@ -6,22 +6,31 @@ class Car {
     
     void printHello();
     int speed(int max_speed);
+    std::string color(const std::string& color);
 };
 
 void Car::printHello() {
-    std::cout << "Hello my Car!!\n";
+    std::cout << "Hello my car BMW!!\n";
 }
 
 int Car::speed(int max_speed) {
     return max_speed;
 }
 
+std::string Car::color(const std::string& color) {
+    return color;
+}
+
 
 int main() {
 
-    Car message;
-    message.printHello();
-    std::cout << "Max speed = " << message.speed(250) << " km.\n";
+    std::string color_user;
+    std::cin >> color_user;
+
+    Car BMW;
+    BMW.printHello();
+    std::cout << "Max speed = " << BMW.speed(250) << " km.\n";
+    std::cout << "Color: " << BMW.color(color_user) << "\n"; 
     
     return 0;
 }
