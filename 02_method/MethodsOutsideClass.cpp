3d2fd59ff66bc57a2d928e5c145641f46ec407ac
RusProgger <ruslantimka92@gmail.com>
2026-08-21 22:22:15 +0300

    Add MethodsOutsideClass

--- 02_method/MethodsOutsideClass.cpp
+++ 02_method/MethodsOutsideClass.cpp
@@ -1,23 +1,27 @@
 #include <iostream>
 #include <string>
-#include <print>
 
 class Car {
     public:
     
     void printHello();
+    int speed(int max_speed);
 };
 
 void Car::printHello() {
     std::cout << "Hello my Car!!\n";
 }
 
+int Car::speed(int max_speed) {
+    return max_speed;
+}
 
 
 int main() {
 
     Car message;
     message.printHello();
+    std::cout << "Max speed = " << message.speed(250) << " km.\n";
     
     return 0;
 }
