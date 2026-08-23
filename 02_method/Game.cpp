8e7104fba2338a7fcfd40709e1b10e4b86913d28
RusProgger <ruslantimka92@gmail.com>
2026-08-23 22:31:36 +0300

    Add project Game practik

---
+++ 02_method/Game.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+
+class Game {
+    private:
+        std::string title;
+        std::string genre;
+        unsigned int rating;
+
+};
+
+
+int main() {
+
+
+
+    return 0;
+}
