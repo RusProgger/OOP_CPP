20bf67c9cc16e6074b32817e8a87793f2c4d3b6b
RusProgger <ruslantimka92@gmail.com>
2026-08-24 10:28:55 +0300

    Add class Smartphone add get_battery

--- practika/Smartphone.cpp
+++ practika/Smartphone.cpp
@@ -7,6 +7,21 @@ class Smartphone {
         int storage;
         int battery;
 
+    public:
+        Smartphone(std::string brand, int storage, int battery) {
+            this->brand = brand;
+            this->storage = storage;
+            this->battery = battery;
+        }
+        
+    int set_battery(int battery) {
+        if(battery > 0 && battery <= 100) {
+            this->battery = battery;
+        } else {
+            std::cout << "Ошибка: заряд должен быть от 0 до 100\n";
+        }
+    }
+
     
 };
 
