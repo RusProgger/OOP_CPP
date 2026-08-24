5160e9c96fda0cb6ef43e4e5dddb7cf3d135eee9
RusProgger <ruslantimka92@gmail.com>
2026-08-24 11:33:47 +0300

    Add getter and setter

--- practika/Book.cpp
+++ practika/Book.cpp
@@ -6,9 +6,35 @@ class Book {
     private:
         std::string title;
         std::string author;
-        unsigned int pages;
+        int pages;
 
-    
+    public:
+        Book(std::string title, std::string author,  int pages) 
+            : title(title), author(author), pages(pages)
+        {
+
+        }
+
+        void set_pages(int pages) {
+            if(pages > 0) 
+                this->pages = pages;
+            else 
+                std::cout << "Ошибка: количество страниц должно быть больше 0";
+        }
+
+        std::string get_title() {
+            return title;
+        }
+
+        std::string get_author() {
+            return author;
+        }
+
+        int get_pages() {
+            return pages;
+        }
+
+        
 };
 
 int main() {
